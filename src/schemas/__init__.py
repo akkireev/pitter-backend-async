307cbd2043ff7ab24a7ae8ceacbfd953e14c061d
Alexander Kireev <alexander_k97@mail.ru>
2019-11-22 11:46:51 +0200

    delete useless functional

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,12 +1,7 @@
 from .message import MessageCreateRequestSchema
 from .message import MessageCreateResponseSchema
 
-APISPEC_DEFAULT_PARAMS = [
-    {'in': 'header', 'name': 'Authorization', 'schema': {'type': 'string'}, 'required': 'true'},
-]
-
 __all__ = [
-    'APISPEC_DEFAULT_PARAMS',
     'MessageCreateRequestSchema',
     'MessageCreateResponseSchema',
 ]
